557233e5cb81cfd8ed339f24c6df4c56cb02b4a8
Your Name <k.bristi057@gmail.com>
2025-06-30 00:02:37 +0600

    added the post method

--- src/app/controller/book.controller.ts
+++ src/app/controller/book.controller.ts
@@ -1,2 +1,17 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { Book } from "../Models/book.models";
 export const routes = express.Router();
+routes.post("/api/books", async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const data = await Book.create(body);
+    res.status(201).json({
+      success: true,
+      message: "Book created successfully!",
+      data,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).json({ success: false, message: error.message, error });
+  }
+});
