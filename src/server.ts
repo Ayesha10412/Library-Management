9bddb96357b77560d92902647672c9108710a98e
Your Name <k.bristi057@gmail.com>
2025-08-09 17:47:15 +0600

    added the updated routes

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 let server: Server;
 const PORT = 5000;
+
 async function main() {
   try {
     await mongoose.connect(process.env.MONGODB_URI as string);
